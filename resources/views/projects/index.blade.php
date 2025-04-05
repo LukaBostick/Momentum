270ca663101528c3481d94ed3fb7066a77f150e8
pmo <lukabostick@my.unt.edu>
2025-04-05 16:44:31 -0500

    button

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,18 +3,26 @@
     <header class="mb-3 flex items-center py-4">
         <div class="flex w-full items-center justify-between">
             <h2 class="text-sm text-gray-500">My Projects</h2>
-            <a href="/projects/create" class="text-grey no-underline">
-                New Project
-            </a>
+            <a href="/projects/create" class="button">New Project</a>
         </div>
     </header>
 
-    <main class="-mx-3 flex flex-wrap">
+    <main class="-mx-3 lg:flex lg:flex-wrap">
         @forelse ($projects as $project)
             <div class="w-1/3 px-3 pb-6">
-                <div class="rounded bg-white p-5 shadow" style="height: 200px">
-                    <h3 class="py-4 text-xl font-normal">
-                        {{ $project->title }}
+                <div
+                    class="rounded bg-white p-5 shadow-lg"
+                    style="height: 200px"
+                >
+                    <h3
+                        class="mb-3 -ml-5 border-l-4 border-b-blue-400 py-4 pl-4 text-xl font-normal"
+                    >
+                        <a
+                            href="{{ $project->path() }}"
+                            class="text-black no-underline"
+                        >
+                            {{ $project->title }}
+                        </a>
                     </h3>
 
                     <div class="text-gray-500">
